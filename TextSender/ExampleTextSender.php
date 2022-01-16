5790556b0c738774549a8f839b1311ea142fc031
KLT <271444+wenqu@users.noreply.github.com>
2022-01-16 15:09:55 +0100

    Update ExampleTextSender.php

--- TextSender/ExampleTextSender.php
+++ TextSender/ExampleTextSender.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Erkens\Security\TwoFactorTextBundle\TextSender;
+namespace Wenqu\Security\TwoFactorTextBundle\TextSender;
 
-use Erkens\Security\TwoFactorTextBundle\Model\TwoFactorTextInterface;
+use Wenqu\Security\TwoFactorTextBundle\Model\TwoFactorTextInterface;
 use Symfony\Component\HttpClient\HttpClient;
 
 class ExampleTextSender implements AuthCodeTextInterface
