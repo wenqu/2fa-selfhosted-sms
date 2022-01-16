d0e0130501aae2750460733d6c03840d1ea73218
KLT <271444+wenqu@users.noreply.github.com>
2022-01-16 15:10:39 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Erkens\Security\TwoFactorTextBundle\DependencyInjection;
+namespace Wenqu\Security\TwoFactorTextBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -21,7 +21,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->canBeEnabled()
             ->children()
-            ->scalarNode('auth_code_sender')->defaultValue('Erkens\Security\TwoFactorTextBundle\TextSender\ExampleTextSender')->end()
+            ->scalarNode('auth_code_sender')->defaultValue('Wenqu\Security\TwoFactorTextBundle\TextSender\ExampleTextSender')->end()
             ->scalarNode('code_generator')->defaultValue('two_factor_text.security.default_code_generator')->end()
             ->scalarNode('template')->defaultValue('@SchebTwoFactor/Authentication/form.html.twig')->end()
             ->integerNode('digits')->defaultValue(6)->min(1)->end()
